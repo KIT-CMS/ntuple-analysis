ef3ab3eff0b021e365a8c5f4e7f6116f5cd6b9bd
Stefan Wunsch <stefan.wunsch@student.kit.edu>
2020-03-25 12:42:08 +0100

    use dataset_from_artusoutput

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 from itertools import product
 
 from ntuple_processor import Histogram
-from ntuple_processor import dataset_from_nameset
+from ntuple_processor import dataset_from_artusoutput
 from ntuple_processor import Unit
 from ntuple_processor import UnitManager
 from ntuple_processor import GraphManager
@@ -34,47 +34,48 @@ def main():
 
     # Define nominal units
     ntuples_base = '/local/scratch/hdd/gallim/ntuples/'
-    friends_base = []
+    friends_base = ['/local/scratch/hdd/gallim/friends/' + f for f in ['SVFit/', 'MELA/', 'FakeFactors/']]
     eras = ['2017']
     channels = ['mt']
     units = {era: {channel: {} for channel in channels} for era in eras}
 
     units['2017']['mt']['data'] = Unit(
-            dataset_from_nameset('data', cfg17.datasets.files['singlemuon'], 'mt_nominal', ntuples_base, friends_base),
+            dataset_from_artusoutput('data', cfg17.datasets.files['singlemuon'], 'mt_nominal', ntuples_base, friends_base),
             [cfg17.channels.mt], hists)
 
-    dy_dataset = dataset_from_nameset('dy', cfg17.datasets.files['DY'], 'mt_nominal', ntuples_base, friends_base)
+    dy_dataset = dataset_from_artusoutput('dy', cfg17.datasets.files['DY'], 'mt_nominal', ntuples_base, friends_base)
     dy_base = [cfg17.channels.mt, cfg17.processes.dy('mt')]
     units['2017']['mt']['ztt'] = Unit(dy_dataset, dy_base + [cfg17.processes.ztt('mt')], hists)
     units['2017']['mt']['zl'] = Unit(dy_dataset, dy_base + [cfg17.processes.zl('mt')], hists)
     units['2017']['mt']['zj'] = Unit(dy_dataset, dy_base + [cfg17.processes.zj('mt')], hists)
 
-    tt_dataset = dataset_from_nameset('tt', cfg17.datasets.files['TT'], 'mt_nominal', ntuples_base, friends_base)
+    tt_dataset = dataset_from_artusoutput('tt', cfg17.datasets.files['TT'], 'mt_nominal', ntuples_base, friends_base)
     tt_base = [cfg17.channels.mt, cfg17.processes.tt('mt')]
     units['2017']['mt']['ttt'] = Unit(tt_dataset, tt_base + [cfg17.processes.ttt('mt')], hists)
     units['2017']['mt']['ttj'] = Unit(tt_dataset, tt_base + [cfg17.processes.ttj('mt')], hists)
     units['2017']['mt']['ttl'] = Unit(tt_dataset, tt_base + [cfg17.processes.ttl('mt')], hists)
 
-    vv_dataset = dataset_from_nameset('vv', cfg17.datasets.files['VV'], 'mt_nominal', ntuples_base, friends_base)
+    vv_dataset = dataset_from_artusoutput('vv', cfg17.datasets.files['VV'], 'mt_nominal', ntuples_base, friends_base)
     vv_base = [cfg17.channels.mt, cfg17.processes.vv('mt')]
     units['2017']['mt']['vvt'] = Unit(vv_dataset, vv_base + [cfg17.processes.vvt('mt')], hists)
     units['2017']['mt']['vvl'] = Unit(vv_dataset, vv_base + [cfg17.processes.vvl('mt')], hists)
     units['2017']['mt']['vvj'] = Unit(vv_dataset, vv_base + [cfg17.processes.vvj('mt')], hists)
 
-    units['2017']['mt']['w'] = Unit(dataset_from_nameset('w', cfg17.datasets.files['W'], 'mt_nominal', ntuples_base, friends_base),
+    units['2017']['mt']['w'] = Unit(dataset_from_artusoutput('w', cfg17.datasets.files['W'], 'mt_nominal', ntuples_base, friends_base),
             [cfg17.channels.mt, cfg17.processes.w('mt')], hists)
 
     units['2017']['mt']['ggh'] = Unit(
-            dataset_from_nameset('ggh', cfg17.datasets.files['ggH'], 'mt_nominal', ntuples_base, friends_base),
+            dataset_from_artusoutput('ggh', cfg17.datasets.files['ggH'], 'mt_nominal', ntuples_base, friends_base),
             [cfg17.channels.mt, cfg17.processes.ggh('mt')], hists)
     units['2017']['mt']['qqh'] = Unit(
-            dataset_from_nameset('qqh', cfg17.datasets.files['qqH'], 'mt_nominal', ntuples_base, friends_base),
+            dataset_from_artusoutput('qqh', cfg17.datasets.files['qqH'], 'mt_nominal', ntuples_base, friends_base),
             [cfg17.channels.mt, cfg17.processes.qqh('mt')], hists)
 
     # Book units with variations
     um = UnitManager()
     um.book([units[era][channel][name] for era, channel, name in
         product(['2017'], ['mt'], ['data', 'ztt', 'zl', 'zj', 'ttt', 'ttj', 'ttl', 'vvt', 'vvj', 'vvl', 'w', 'ggh', 'qqh'])])
+    """
     um.book([units[era][channel][name] for era, channel, name in
         product(['2017'], ['mt'], ['data', 'ztt', 'zl', 'zj', 'ttt', 'ttj', 'ttl', 'vvt', 'vvj', 'vvl', 'w', 'ggh', 'qqh'])],
         [cfg17.variations.same_sign])
@@ -107,15 +108,16 @@ def main():
         [*cfg17.variations.top_pt])
     um.book([units[era][channel][name] for era, channel, name in product(['2017'], ['mt'], ['ggh'])],
         [*cfg17.variations.ggh_theory])
+    """
 
     # Optimize graphs
     g_manager = GraphManager(um.booked_units, True)
-    g_manager.optimize(1)
+    g_manager.optimize(2)
     graphs = g_manager.graphs
 
     # Run computations
     r_manager = RunManager(graphs)
-    r_manager.run_locally('output.root', nworkers=6, nthreads=2)
+    r_manager.run_locally('output.root', nworkers=12, nthreads=1)
 
 
 if __name__ == "__main__":
